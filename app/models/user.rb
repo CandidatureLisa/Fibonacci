4f6027ba69d08d83dcf0bbb0d6ff1abc3e7f7f3f
CandidatureLisa <lisa@webibli.fr>
2017-10-19 20:33:49 +0200

    add new fields to users table and set validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  SITUATIONS = ['I move in', 'I built a new flat/house', 'I need a temporary access']
+
+  validates :first_name, :last_name, :street_number, :street_name, :zip_code, :city, :situation, :pdl, presence: true
+  validates :situation, inclusion: { in: SITUATIONS }
+  validates :pdl,  numericality: { only_integer: true }, length: { is: 14 }
 end
